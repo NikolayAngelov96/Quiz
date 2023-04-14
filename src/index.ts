f1fbd69500005552e57a5b3ce5f12ceb8cc23d71
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2023-04-14 10:37:54 +0300

    load questions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-alert("it works");
+import {Quiz} from "./Quiz";
+
+const backgroundElement = document.querySelector(".bg") as HTMLElement
+
+async function loadQuestions() {
+    const res = await fetch("./data/questions.json");
+
+    if(!res.ok) {
+        throw new Error("Can't load questions")
+    }
+
+    return await res.json();
+}
+
+async function startQuiz() {
+    const data = await loadQuestions();
+
+    new Quiz(data, backgroundElement);
+}
+
+startQuiz();
