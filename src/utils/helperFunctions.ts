26236909c48962be338df290ffbd8d6433a91168
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2023-04-30 10:14:37 +0300

    fix bug with event listeners

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -3,11 +3,12 @@ type ElementType = keyof HTMLElementTagNameMap;
 export function createElement<T>(type: ElementType, props?: Record<string, any>, ...content: any[]) {
     const element = document.createElement(type);
 
+    console.log(props);
     if (props) {
         for (let propName in props) {
             if (propName.startsWith("on")) {
                 const eventName: string = propName.slice(2).toLowerCase();
-                element.addEventListener(eventName, props[eventName]);
+                element.addEventListener(eventName, props[propName]);
             } else if (propName == "style") {
                 for (let entry in props.style) {
                     // @ts-ignore
